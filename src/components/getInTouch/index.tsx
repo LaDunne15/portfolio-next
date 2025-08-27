0807118d5644ae5172980132c09e45d86845a508
Vladyslav Koshelnyi <vkoshelnyi@salerow.software>
2025-08-28 00:00:59 +0300

    fix style project card

--- src/components/getInTouch/index.tsx
+++ src/components/getInTouch/index.tsx
@@ -9,20 +9,22 @@ import s from '@/styles/Home.module.scss';
 import { Send } from 'lucide-react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
+import { useBreakpointValue } from '@/hooks/useBreakpointValue';
 
 export const GetInTouch = () => {
   const t = useTranslations('main');
+  const radius = useBreakpointValue({ base: 10, md: 20, lg: 40 });
 
   return (
-    <GlassElement width="100%" height="100%" radius={30} depth={0} chromaticAberration={5}>
+    <GlassElement width="100%" height="100%" radius={radius} depth={0} chromaticAberration={5}>
       <div className={s.getInTouch}>
         <div className={s.getInTouch_main}>
-          <span className={s.getInTouch_title}>{t('Get in touch')}</span>
-          <span className={s.getInTouch_text}>{t('Get in touch text')}</span>
+          <h2>{t('Get in touch')}</h2>
+          <h3>{t('Get in touch text')}</h3>
         </div>
         <div className={s.getInTouch_actions}>
           <div className={s.getInTouch_followMe}>
-            <span className={s.getInTouch_followMe_title}>{t('Follow me on')}</span>
+            <span className={s.getInTouch_followMe_title}>{t('Follow me on')}:</span>
             <div className={s.getInTouch_followMe_links}>
               <Link href="/" className="link">
                 <Image src={GitHub} alt="github" className={s.linkIcon} />
@@ -35,11 +37,13 @@ export const GetInTouch = () => {
               </Link>
             </div>
           </div>
-          <div className={s.contactMe}>
+          <div className={s.getInTouch_contact}>
             <Link href="/contact" className="link">
-              <div className={s.contactMe_content}>
-                <span>{t('Contact me')}</span>
-                <Send />
+              <div className={s.contactMe}>
+                <div className={s.contactMe_content}>
+                  <span>{t('Contact me')}</span>
+                  <Send />
+                </div>
               </div>
             </Link>
           </div>
--- src/components/projectCard/index.tsx
+++ src/components/projectCard/index.tsx
@@ -16,27 +16,26 @@ export const ProjectCard = ({ project: i }: Props) => {
   const radius = useBreakpointValue({ base: 10, md: 20, lg: 40 });
 
   return (
-    <GlassElement width="100%" height="100%" radius={radius} depth={1}>
-      <Link href={`/projects/${i.slug}`} className="link">
-        <div
-          className={s.projectCard}
-          style={{ '--main-color': i.mainColor } as React.CSSProperties}
-        >
-          <div className={s.projectCard_imageContainer}>
-            <Image src={i.mainImage} alt="" className={s.projectCard_image} />
-          </div>
-          <div className={s.projectCard_info}>
-            <h3>{i.title}</h3>
-            <h4>{i.devDirection}</h4>
-            <h5>{i.shortDescription}</h5>
-            <div style={{ display: 'flex', gap: '10px', marginTop: 10 }}>
-              {i.stack.map((j) => (
-                <TechItem name={j} key={`${i.id}${j}`} size={30} />
-              ))}
+    <Link href={`/projects/${i.slug}`} className="link">
+      <div className={s.projectCard} style={{ '--main-color': i.mainColor } as React.CSSProperties}>
+        <GlassElement width="100%" height="100%" radius={radius} depth={1}>
+          <div className={s.projectCard}>
+            <div className={s.projectCard_imageContainer}>
+              <Image src={i.mainImage} alt="" className={s.projectCard_image} />
+            </div>
+            <div className={s.projectCard_info}>
+              <h3>{i.title}</h3>
+              <h4>{i.devDirection}</h4>
+              <h5 className="flex">{i.shortDescription}</h5>
+              <div style={{ display: 'flex', gap: '10px', marginTop: 10 }}>
+                {i.stack.map((j) => (
+                  <TechItem name={j} key={`${i.id}${j}`} size={30} />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-    </GlassElement>
+        </GlassElement>
+      </div>
+    </Link>
   );
 };
